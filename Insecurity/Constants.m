e056a1ae555d0e516728863ed54282a52479cd9b
Alan Scarpa <alan.scarpa@gmail.com>
2015-08-05 11:12:13 -0400

    Fixing

---
+++ Insecurity/Constants.m
@@ -0,0 +1,16 @@
+//
+//  Constants.m
+//  Insecurity
+//
+//  Created by Alan Scarpa on 8/5/15.
+//  Copyright (c) 2015 Skytop Designs. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+NSString *const PARSE_APPLICATION_ID = @"H7F54iqLlrQR705dKcOQ1YnafZfj1U5gzPlTPCwt";
+NSString *const PARSE_CLIENT_KEY = @"ZIXPHQy5v2exIe3yZqdGrwgTkBZ708YL7dfOSqt9";
+
+@end
