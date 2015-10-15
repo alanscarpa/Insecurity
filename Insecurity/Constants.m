2e902818889e8ec0c203fc83cf52073cfe1aa6b0
Alan Scarpa <alan.scarpa@gmail.com>
2015-10-15 07:11:21 -0400

    Added chartboost keys

--- Insecurity/Constants.m
+++ Insecurity/Constants.m
@@ -12,6 +12,6 @@
 
 NSString *const PARSE_APPLICATION_ID = @"H7F54iqLlrQR705dKcOQ1YnafZfj1U5gzPlTPCwt";
 NSString *const PARSE_CLIENT_KEY = @"ZIXPHQy5v2exIe3yZqdGrwgTkBZ708YL7dfOSqt9";
-NSString *const CHARTBOOST_APP_ID = @"";
-NSString *const CHARTBOOST_APP_KEY = @"";
+NSString *const CHARTBOOST_APP_ID = @"561af74bf6cd455396e15083";
+NSString *const CHARTBOOST_APP_KEY = @"c5a2869f1ed94f16d4414c2e0f5241cbbcfd7c6b";
 @end
